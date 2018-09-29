07a8bf98390c1812d79d8057b018ab679968297e
anweer <43642999+anweer@users.noreply.github.com>
2018-09-29 10:48:32 +0300

    Update RUNFA.sh

--- RUNFA.sh
+++ RUNFA.sh
@@ -8,22 +8,16 @@ install() {
 wget "https://valtman.name/files/telegram-cli-1222"
 mv telegram-cli-1222 tg
 sudo chmod +x tg
-echo -e "
- __   ___________   ___     _____     __  __
-/  |  |___   ___|  / _ \   | ___ }   |  \/  |
-\_ \      | |     | | | |  | |_) }   | |\/| |
- _) |     | |     | |_| |  |  _< \   | |  | |
-|__/      |_|      \___/   |_|  \_\  |_|  |_|
- BY @TAHAJ20 DEV : TAHAJ20            "
-echo -e "" 
-echo -e "" 
+echo -e " BY TEAM FAEDER"
+echo -e " BY TEAM FAEDER" 
+echo -e " BY TEAM FAEDER" 
 }
 if [ "$1" = "install" ]; then
 install
 else
 if [ ! -f ./tg ]; then
-echo "" 
-echo ""
+echo " BY TEAM FAEDER" 
+echo " BY TEAM FAEDER"
 exit 1
 fi
 ./tg -s FAEDER.lua
