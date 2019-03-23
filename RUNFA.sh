80ae38b160fbb06c8f17c6630aee0f4628cc6b0f
anweer <43642999+anweer@users.noreply.github.com>
2019-03-23 06:56:49 +0300

    Update RUNFA.sh

--- RUNFA.sh
+++ RUNFA.sh
@@ -1,20 +1,170 @@
+!/usr/bin/env bash
+
+
 THIS_DIR=$(cd $(dirname $0); pwd)
+
 cd $THIS_DIR
+
+
 install() {
-wget "https://valtman.name/files/telegram-cli-1222"
+
+cd tg
+
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+
+sudo apt-get install g++-4.7 -y c++-4.7 -y
+
+sudo apt-get update
+
+sudo apt-get upgrade
+
+sudo apt-get install libreadline-dev -y libconfig-dev -y libssl-dev -y lua5.2 -y liblua5.2-dev -y lua-socket -y lua-sec -y lua-expat -y libevent-dev -y make unzip git redis-server autoconf g++ -y libjansson-dev -y libpython-dev -y expat libexpat1-dev -y
+
+sudo apt-get install screen -y
+
+sudo apt-get install tmux -y
+
+sudo apt-get install libstdc++6 -y
+
+sudo apt-get install lua-lgi -y
+
+sudo apt-get install libnotify-dev -y
+
+wget https://valtman.name/files/telegram-cli-1222
+
 mv telegram-cli-1222 tg
+
 sudo chmod +x tg
-echo -e " BY TEAM FAEDER"
-echo -e " BY TEAM FAEDER" 
-echo -e " BY TEAM FAEDER" 
+
+cd ..
+
+
+chmod +x tg
+
+chmod +x autofaeder.sh
+
 }
+
+
+function print_logo() {
+
+green " _              _                 _"
+            |  _|_ _  _  | | _ _     | |_ 
+
+green "| |_ / _ |/ _ \/ _ |/ _ \ '|  / _ \ \/ / "
+
+green "|  _| (_| |  / (_| |  / |    | (_| |>  <"
+
+green "|_|  \,_|\_|\__,_|\___|_|     \__,_/_/\_\"
+
+echo -e "\n\e[0m"
+
+}
+
+
+function logo_play() {
+
+declare -A txtlogo
+
+seconds="0.010"
+txtlogo[1]=" _              _                 _"
+                   |  _|_ _  _  | | _ _     | |_ 
+
+txtlogo[2]="| |_ / _ |/ _ \/ _ |/ _ \ '|  / _ \ \/ / "
+
+txtlogo[3]="|  _| (_| |  / (_| |  / |    | (_| |>  <"
+
+txtlogo[4]="|_|  \,_|\_|\__,_|\___|_|     \__,_/_/\_\"
+
+printf "\e[31m\t"
+
+for i in ${!txtlogo[@]}; do
+
+for x in seq 0 ${#txtlogo[$i]}; do
+
+printf "${txtlogo[$i]:$x:1}"
+
+sleep $seconds
+
+done
+
+printf "\n\t"
+
+done
+
+printf "\n"
+
+echo -e "\e[0m"
+
+}
+
+
+function beyondteam() {
+
+echo -e "\e[0m"
+
+green " >>>> I'm not the only one. "
+
+green " >>>> But I'm the best "
+
+white " >>>> I'm the only programmer"
+
+white " >>>> To a source FAEDER"
+
+red " >>>> Follow my program"
+
+red " >>>> @Faeder_ch "
+
+echo -e "\e[0m"
+
+}
+
+
+red() {
+
+printf '\e[1;31m%s\n\e[0;39;49m' "$@"
+
+}
+
+green() {
+
+printf '\e[1;32m%s\n\e[0;39;49m' "$@"
+
+}
+
+white() {
+
+printf '\e[1;37m%s\n\e[0;39;49m' "$@"
+
+}
+
+update() {
+
+git pull
+
+}
+
+
 if [ "$1" = "install" ]; then
+
+print_logo
+
+faeder
+
+logo_play
+
 install
-else
+
+ else
+
 if [ ! -f ./tg ]; then
-echo " BY TEAM FAEDER" 
-echo " BY TEAM FAEDER"
+
+echo "tg not found"
+
+echo "Run $0 install"
+
 exit 1
-fi
-./tg -s FAEDER.lua
+
+ fi
+ ./tg -s FAEDER.lua
 fi
