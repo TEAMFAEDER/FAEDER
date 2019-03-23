cf97a724455954542ee75c6db6b2175c659a3b28
anweer <43642999+anweer@users.noreply.github.com>
2019-03-23 07:56:24 +0300

    Update RUNFA.sh

--- RUNFA.sh
+++ RUNFA.sh
@@ -1,41 +1,174 @@
+!/usr/bin/env bash
+
+
 THIS_DIR=$(cd $(dirname $0); pwd)
+
 cd $THIS_DIR
+
+
 install() {
-cd tg
 
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+  cd tg
 
-sudo apt-get install g++-4.7 -y c++-4.7 -y
+  sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 
-sudo apt-get update
+  sudo apt-get install g++-4.7 -y c++-4.7 -y
 
-sudo apt-get upgrade
+  sudo apt-get update
 
-sudo apt-get install libreadline-dev -y libconfig-dev -y libssl-dev -y lua5.2 -y liblua5.2-dev -y lua-socket -y lua-sec -y lua-expat -y libevent-dev -y make unzip git redis-server autoconf g++ -y libjansson-dev -y libpython-dev -y expat libexpat1-dev -y
+  sudo apt-get upgrade
 
-sudo apt-get install screen -y
+  sudo apt-get install libreadline-dev -y libconfig-dev -y libssl-dev -y lua5.2 -y liblua5.2-dev -y lua-socket -y lua-sec -y lua-expat -y libevent-dev -y make unzip git redis-server autoconf g++ -y libjansson-dev -y libpython-dev -y expat libexpat1-dev -y
 
-sudo apt-get install tmux -y
+  sudo apt-get install screen -y
 
-sudo apt-get install libstdc++6 -y
+  sudo apt-get install tmux -y
 
-sudo apt-get install lua-lgi -y
+  sudo apt-get install libstdc++6 -y
 
-sudo apt-get install libnotify-dev -y
-wget "https://valtman.name/files/telegram-cli-1222"
-mv telegram-cli-1222 tg
-sudo chmod +x tg
-echo -e " BY TEAM FAEDER"
-echo -e " BY TEAM FAEDER" 
-echo -e " BY TEAM FAEDER" 
+  sudo apt-get install lua-lgi -y
+
+  sudo apt-get install libnotify-dev -y
+
+  wget https://valtman.name/files/telegram-cli-1222
+
+  mv telegram-cli-1222 tg
+
+  sudo chmod +x tg
+
 }
+
+
+function print_logo() {
+
+ green " ____ ____ _"
+
+ green " | _ )| _ \ |_ _|___ ____  "
+
+ green " | _ \| |_) ) | |/ .__| _ \_| \/ |"
+
+ green " |____/|____/ |_|\____/\_____|_/\/\_|"
+
+ echo -e "\n\e[0m"
+
+}
+
+
+function logo_play() {
+
+    declare -A txtlogo
+
+    seconds="0.010"
+
+    txtlogo[1]=" ____ ____ _"
+
+    txtlogo[2]="| _ )| _ \ |_ _|___ ____  "
+
+    txtlogo[3]="| _ \| |_) ) | |/ .__| _ \_| \/ |"
+
+    txtlogo[4]="|____/|____/ |_|\____/\_____|_/\/\_|"
+
+    printf "\e[31m\t"
+
+    for i in ${!txtlogo[@]}; do
+
+        for x in seq 0 ${#txtlogo[$i]}; do
+
+            printf "${txtlogo[$i]:$x:1}"
+
+            sleep $seconds
+
+        done
+
+        printf "\n\t"
+
+    done
+
+    printf "\n"
+
+ echo -e "\e[0m"
+
+}
+
+
+function FAEDER() {
+
+ echo -e "\e[0m"
+
+ green " >>>> We Are Not Attacker "
+
+ green " >>>> We Are Not Alliance "
+
+ white " >>>> We Are Programmer "
+
+ white " >>>> We Are The Best "
+
+ red " >>>> We Are Family "
+
+ red " >>>> @FAEDER "
+
+ echo -e "\e[0m"
+
+}
+
+
+red() {
+
+  printf '\e[1;31m%s\n\e[0;39;49m' "$@"
+
+}
+
+green() {
+
+  printf '\e[1;32m%s\n\e[0;39;49m' "$@"
+
+}
+
+white() {
+
+  printf '\e[1;37m%s\n\e[0;39;49m' "$@"
+
+}
+
+update() {
+
+ git pull
+
+}
+
+
 if [ "$1" = "install" ]; then
-install
-else
+
+ print_logo
+
+ FAEDER
+
+ logo_play
+
+ install
+
+  else
+
 if [ ! -f ./tg ]; then
-echo " BY TEAM FAEDER" 
-echo " BY TEAM FAEDER"
-exit 1
-fi
-./tg -s FAEDER.lua
+
+    echo "tg not found"
+
+    echo "Run $0 install"
+
+    exit 1
+
+ fi
+
+ print_logo
+
+ FAEDER
+
+ logo_play
+
+   #sudo service redis-server restart
+
+   ./tg -s ./FAEDER.lua -l 1 -E $@
+
+   #./tg -s ./FAEDER.lua $@
+
 fi
