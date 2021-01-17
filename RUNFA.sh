0bf79495ea6f502effd0a0c83771fee4bc8a7e7a
Faeder Dx <43642999+anweer@users.noreply.github.com>
2021-01-17 22:01:14 +0300

    Update RUNFA.sh

--- RUNFA.sh
+++ RUNFA.sh
@@ -15,31 +15,62 @@ echo ""
 echo ""     •{ Setting up offices }•
 exit 1
 fi
-
-apt update
-apt upgrade
+echo -e "\e[38;5;77m    •{ Welcome to install source Faeder }•\e[0m"
+echo -e "\e[38;5;77m    •{ Setting up offices }•\e[0m"
+echo -e "\e[38;5;77m       •{ install source Faeder [0%]  }•\e[0m"
 sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install tmux
-sudo apt-get install luarocks
-sudo apt-get install screen
-sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev
-sudo apt-get update
-sudo apt-get install
-sudo apt-get install upstart-sysv
-wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz;tar zxpf luarocks-2.2.2.tar.gz;cd luarocks-2.2.2 && ./configure
-sudo make bootstrap
-sudo luarocks install luasocket
-sudo luarocks install luasec
-sudo apt-get install libconfig++9v5 -y 
-sudo apt-get install libstdc++6 -y
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y 
-sudo apt-get install lua-lgi -y  
-sudo apt-get install libnotify-dev -y 
-sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev -y
-sudo apt-get update 
 sudo apt-get upgrade -y
 
+echo -e "\e[38;5;77m       •{ install source Faeder [10%]  }•\e[0m"
+
+sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev  -y
+sudo apt-get install lua-lgi -y
+
+echo -e "\e[38;5;77m       •{ install source Faeder [20%]  }•\e[0m"
+
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo apt-get install libstdc++6 -y
+
+echo -e "\e[38;5;77m       •{ install source Faeder [30%]  }•\e[0m"
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+echo -e "\e[38;5;77m       •{ install source Faeder [40%]  }•\e[0m"
+
+sudo luarocks install luasocket
+sudo luarocks install luasecreset
+
+echo -e "\e[38;5;77m       •{ install source Faeder [50%]  }•\e[0m"
+
+sudo luarocks install redis-lua
+sudo luarocks install lua-term
+sudo luarocks install serpent
+
+echo -e "\e[38;5;77m       •{ install source Faeder [60%]  }•\e[0m"
+
+sudo luarocks install dkjson
+sudo luarocks install Lua-cURL
+
+echo -e "\e[38;5;77m       •{ install source Faeder [70%]  }•\e[0m"
+
+sudo service redis-server start
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get install g++-4.7 -y c++-4.7
+
+echo -e "\e[38;5;77m       •{ install source Faeder [80%]  }•\e[0m"
+
+sudo apt-get install libreadline-dev -y libconfig-dev -y libssl-dev -y lua5.2 -y liblua5.2-dev -y lua-socket -y lua-sec -y lua-expat -y libevent-dev -y make unzip git redis-server autoconf g++ -y libjansson-dev -y libpython-dev -y expat libexpat1-dev -y
+sudo apt-get install screen -y
+sudo apt-get install tmux -y
+
+echo -e "\e[38;5;77m       •{ install source Faeder [90%]  }•\e[0m"
+
+sudo apt-get install libstdc++6 -y
+sudo apt-get install lua-lgi -y
+sudo apt-get install libnotify-dev -y
+ 
 echo -e "\e[38;5;77m       •{ install source Faeder [100%]  }•\e[0m"
 echo -e "\e[38;5;77m       •{ •{ Join the Source Channel : @faeder_ch }• }•\e[0m"
 
