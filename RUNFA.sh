61c982588c5d76b00e10613913d6a4aaa8f482c2
Faeder Dx <43642999+anweer@users.noreply.github.com>
2021-02-26 22:50:41 +0300

    Update RUNFA.sh

--- RUNFA.sh
+++ RUNFA.sh
@@ -66,7 +66,7 @@ echo -e "\e[38;5;77m Installing depedencies for ubuntu 18  ... \e[0m"
 cd /lib/x86_64-linux-gnu/ && sudo ln -s libreadline.so.7.0 libreadline.so.6
 wget "ibotcorp.com/files/compat-libevent2-5_2.0.21-1ubuntu18_amd64.deb" && sudo dpkg -i compat-libevent2-5_2.0.21-1ubuntu18_amd64.deb
 rm compat-libevent2-5_2.0.21-1ubuntu18_amd64.deb
-if
+fi
 }
 if [ "$1" = "install" ]; then
 install
